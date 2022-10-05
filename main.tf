9de5c50c854b04bea2ea908835d29cd7e41a9f63
AntoineCKuhn <97163710+AntoineCKuhn@users.noreply.github.com>
2022-10-05 13:14:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,33 @@ module "landingzone" {
   tfc_org_name = var.tfc_org_name
   oauth_token_id = var.oauth_token_id
 }
+
+  
+variable "tfc_hostname" {
+  description = "Hostname of TFC/TFE"
+  default = "app.terraform.io"
+}
+
+variable "tfc_token" {
+  description = "TFC/E API Token"
+}
+
+variable "github_token" {
+  description = "Github API Token to be able to create repos"
+}
+
+variable "github_repo_name" {
+  description = "Name for the Github repo"
+}
+
+variable "tfc_workspace_name" {
+  description = "Name for the TFC/E workspace"
+}
+
+variable "tfc_org_name" {
+  description = "Name of the TFC/E Organization"
+}
+
+variable "oauth_token_id" {
+  description = "Can be found in TFC/E under General Settings -> Provider"
+}
