288979e553f987177387333f426a513385eb3f7f
AntoineCKuhn <97163710+AntoineCKuhn@users.noreply.github.com>
2022-11-21 17:17:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "landingzone" {
   source  = "app.terraform.io/hashicorp-antoine/landingzone/tfe"
-  version = "1.0.5"
+  version = "1.0.6"
   
   tfc_token = var.tfc_token
   github_token = var.github_token
